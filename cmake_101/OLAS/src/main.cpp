2404b79c79c9af93a528f854c53a8a682c26f443
Lawjune <lawjune@163.com>
2023-01-29 08:10:14 +0800

    Added cmake_101.

---
+++ cmake_101/OLAS/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "adder.h"
+
+
+int main(int argc, char* argv[]) 
+{
+    std::cout << "Hey, Zues!\n";
+
+    std::cout << add(72.1f, 73.8f) << '\n';
+
+    return 0;
+}
---
+++ sdl2_demo/src/main.cpp
@@ -0,0 +1,78 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+
+#define FILE_PATH "./res/sample.wav"
+
+struct AudioData
+{
+	uint8_t *pos;
+	uint32_t length;
+};
+
+void MyAudioCallback(void* userdata, uint8_t* stream, int streamLength)
+{
+	AudioData *audio = (AudioData *)userdata;
+
+	if (audio->length == 0)
+	{
+		return;
+	}
+
+	uint32_t length = (uint32_t)streamLength;
+	length = (length > audio->length ? audio->length : length);
+
+	SDL_memcpy(stream, audio->pos, length);
+	audio->pos += length;
+	audio->length -= length;
+}
+
+int main(int argc, char **argv)
+{
+	SDL_Init(SDL_INIT_AUDIO);
+
+	SDL_AudioSpec wavSpec;
+	uint8_t *wavStart;
+	uint32_t wavLength;
+
+	if(SDL_LoadWAV(FILE_PATH, &wavSpec, &wavStart, &wavLength) == NULL)
+	{
+		// TODO: Proper error handling
+		std::cerr << "Error: " << FILE_PATH
+				  << " could not be loaded as an audio file" << std::endl;
+		return 1;
+	}
+
+	AudioData audio;
+	audio.pos = wavStart;
+	audio.length = wavLength;
+
+	wavSpec.callback = MyAudioCallback;
+	wavSpec.userdata = &audio;
+
+	SDL_AudioDeviceID device = SDL_OpenAudioDevice(NULL, 
+													0, 
+													&wavSpec, 
+													NULL, 
+													SDL_AUDIO_ALLOW_ANY_CHANGE);
+
+	if (device == 0)
+	{
+		// TODO: Proper error handling
+		std::cerr << "Error" << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	SDL_PauseAudioDevice(device, 0);
+	while(audio.length > 0)
+	{
+		SDL_Delay(100);
+	}
+
+	std::cout << "Hello World!" << std::endl;
+
+	SDL_CloseAudioDevice(device);
+	SDL_FreeWAV(wavStart);
+	SDL_Quit();
+
+	return 0;
+}
