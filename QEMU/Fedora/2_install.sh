ac8cfcf08c0745f53f199a1f79926f3d81a81f29
JunLuo <lawjune@163.com>
2023-05-03 15:30:19 +0800

    Added qemu usage.

--- QEMU/Fedora/2_install.sh
+++ QEMU/Fedora/2_install.sh
@@ -3,7 +3,7 @@
 qemu-system-x86_64 \
 -m 8G \
 -smp 6 \
--cdrom /Volumes/Samsung_T5/iso/Fedora-Workstation-Live-x86_64-35-1.2.iso \
+-cdrom Fedora-Workstation-Live-x86_64-34-1.2.iso \
 -drive file=mydisk.qcow2,if=virtio \
 -vga virtio \
 -display default,show-cursor=on \
--- QEMU/qemu.md
+++
@@ -1,7 +0,0 @@
-```sh
-qemu-img create -f qcow2 manjaro.qcow2 1.5G
-ls -la
-mv ~/Downloads/manjaro-gnome-22.1.0-230421-linux61.iso .
-ls -la
-touch start-manjaro.sh
-```
